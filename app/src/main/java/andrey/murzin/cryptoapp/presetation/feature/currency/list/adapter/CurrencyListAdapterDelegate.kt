0acaa4bb1dbcce69caa46b23eb78477db5b10ff6
AndreyMurzin <lefthanderomg@gmail.com>
2019-09-11 18:53:26 +0300

    add price

--- app/src/main/java/andrey/murzin/cryptoapp/presetation/feature/currency/list/adapter/CurrencyListAdapterDelegate.kt
+++ app/src/main/java/andrey/murzin/cryptoapp/presetation/feature/currency/list/adapter/CurrencyListAdapterDelegate.kt
@@ -3,12 +3,13 @@ package andrey.murzin.cryptoapp.presetation.feature.currency.list.adapter
 import andrey.murzin.cryptoapp.R
 import andrey.murzin.cryptoapp.domain.entity.CoinEntity
 import andrey.murzin.cryptoapp.presetation.feature.tool.ext.inflate
+import andrey.murzin.cryptoapp.presetation.feature.tool.ext.load
 import andrey.murzin.cryptoapp.presetation.feature.tool.ext.toImageUrl
+import andrey.murzin.cryptoapp.presetation.feature.tool.ext.toPrice
 import android.content.Context
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
 import com.hannesdorfmann.adapterdelegates4.AdapterDelegate
 import kotlinx.android.extensions.LayoutContainer
 import kotlinx.android.synthetic.main.item_currency.view.*
@@ -38,8 +39,11 @@ class CurrencyListAdapterDelegate @Inject constructor(
         fun bind(item: CoinEntity) {
             with(containerView) {
                 tvRank.text = item.cmcRank?.toString() ?: ""
+                tvCoinName.text = item.name ?: ""
+                tvCoinSymbol.text = item.symbol ?: ""
+                tvPrice.text = item.quote?.usd?.price?.toPrice() ?: ""
                 item.id?.toImageUrl()?.let { umgUrl ->
-                    Glide.with(parentContext).load(umgUrl).into(imgIcon)
+                    imgIcon.load(context, umgUrl)
                 }
             }
         }
--- app/src/main/java/andrey/murzin/cryptoapp/presetation/feature/tool/ext/ImageUrlExt.kt
+++
@@ -1,6 +0,0 @@
-package andrey.murzin.cryptoapp.presetation.feature.tool.ext
-
-private const val IMAGE_URL_32 = "https://s2.coinmarketcap.com/static/img/coins/32x32/%d.png"
-
-fun Int.toImageUrl() : String =
-    IMAGE_URL_32.format(this)
